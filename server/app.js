e5761fd3c78388df70d28b1560018608573af64c
Muhammad <myhoomac@hotmail.com>
2019-04-04 23:58:30 +0700

    please be careful to use, check your work before commit, OK ?

--- server/app.js
+++ server/app.js
@@ -1,19 +1,30 @@
-require("env").config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
+const uri = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${
+  process.env.MONGO_DB_PIN
+}@cluster0-nktui.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true`;
+
+mongoose
+  .connect(
+    uri,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log(`======> MongoDB connected <======`))
+  .catch(err => console.log(err, "ini error"));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
-// routes go here 
+// routes go here
 // app.use( '/', routes )
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`connected with http://localhost:3000`);
-);
+});
