dc8e35e42a48020fa0d2156c02afd6b87af3a101
paulinavita <paulinadavita@gmail.com>
2019-04-04 23:59:59 +0700

    revise routings

--- server/app.js
+++ server/app.js
@@ -1,19 +1,23 @@
-require("env").config();
+require('dotenv').config()
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const userRoutes = require('./routes/user')
+const wikiRoutes = require('./routes/wiki')
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-
 // routes go here 
-// app.use( '/', routes )
+app.use( '/', userRoutes)
+// app.use( '/', wikiRoutes)
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`connected with http://localhost:3000`);
-);
+});
