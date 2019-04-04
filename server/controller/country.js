1b27577133835ec3fca6dd23d5bbd289cdff0fab
paulinavita <paulinadavita@gmail.com>
2019-04-05 01:23:42 +0700

    add function filter region

--- server/controller/country.js
+++ server/controller/country.js
@@ -8,11 +8,27 @@ class CountryController {
     static getCountries (req, res) {
         country.get('/all')
         .then(({data}) => {
+            console.log('bisa dapat data all')
             res.status(200).json(data)
         })
         .catch(err => {
+            console.log('err bagian get country')
             res.status(400).json({err : err.message})
         })
+    }
+
+    static getByRegion(req, res) {
+        country.get(`/region/${req.params.region}`)
+        .then(({data}) => {
+            console.log(req.params.region)
+            console.log('bisa dapat data region')
+            res.status(200).json(data)
+        })
+        .catch(err => {
+            console.log('err bagian get region')
+            res.status(400).json({err : err.message})
+        })
+
     }
 
 }
