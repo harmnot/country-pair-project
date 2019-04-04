59444d69a41e062e4fefa3797e0b60347260302c
Muhammad <myhoomac@hotmail.com>
2019-04-05 06:47:48 +0700

    controller and routes for WIKI

--- server/routes/wiki.js
+++ server/routes/wiki.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const Wikipedia = require("../controller/wikipedia.js");
+
+router.get("/:search", Wikipedia.wiki);
 
 // middleware for checking if ERROR
-app.use((err, req, res, next) => {
+router.use((err, req, res, next) => {
   if (err) {
     res.status(500).json(err);
   }
