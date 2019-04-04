fdac0c6e239a1435522a356404179736857852bb
yogapermana76 <permana76.yp@gmail.com>
2019-04-05 02:10:48 +0700

    add function onSignIn

--- client/main.js
+++ client/main.js
@@ -1,3 +1,27 @@
+function onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    const id_token = googleUser.getAuthResponse().id_token;
+
+    
+    $.ajax({
+        url: 'http://localhost:3000/login-google',
+        type: 'POST',
+        data: {
+            id_token
+        }
+    })
+    .done(function(response) {
+        localStorage.setItem('token', response)
+    })
+    .fail(err => {
+        console.log(err)
+    })
+}
+
 function fetchAllCountries() {
     $.ajax({
         url : 'http://localhost:3000/countries',
